706616f0bc693c6809eb5e8a72d197a564e90357
Andrew Hall (DEVDIV) <andrehal@microsoft.com>
2018-05-05 19:48:09 -0700

    Updating BusRouteProvider

--- BusInfo.Web/BusRouteProvider.cs
+++ BusInfo.Web/BusRouteProvider.cs
@@ -10,16 +10,12 @@ namespace BusInfo.Web
     {
         public async Task<T> GetBusRoutesAsync<T>(string url)
         {
-            //WebRequest request = WebRequest.CreateHttp(url);
-            //var results = await request.GetResponseAsync();
-            //var response = results.GetResponseStream();
-            //var sr = new StreamReader(response);
-            //var json = await sr.ReadToEndAsync();
-            //var routes = JsonConvert.DeserializeObject<T>(json);
-            //return routes;
-            var routesMock = new MockBusLocator();
-            var routesJson = await routesMock.GetJsonForStopsAsync();
-            T routes = JsonConvert.DeserializeObject<T>(routesJson);
+            WebRequest request = WebRequest.CreateHttp(url);
+            var results = await request.GetResponseAsync();
+            var response = results.GetResponseStream();
+            var sr = new StreamReader(response);
+            var json = await sr.ReadToEndAsync();
+            var routes = JsonConvert.DeserializeObject<T>(json);
             return routes;
         }
     }
