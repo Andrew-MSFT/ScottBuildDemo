c81be5f5e05f79a9d2bd23278a2ce2a309e0364e
Andrew Hall (DEVDIV) <andrehal@microsoft.com>
2018-05-05 19:53:54 -0700

    Updating formatting of Index.cshtml.cs

---
+++ BusInfo.Web/DisplayRouteData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BusInfo.Web
+{
+    public class DisplayRouteData
+    {
+        public string Description { get; set; }
+        public string ShortName { get; set; }
+        public DateTime PredictedArrival { get; set; }
+        public string DisplayArrivalTime
+        {
+            get
+            {
+                return this.PredictedArrival.ToShortTimeString();
+            }
+        }
+    }
+}
--- BusInfo.Web/Pages/Index.cshtml.cs
+++ BusInfo.Web/Pages/Index.cshtml.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -7,19 +6,6 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BusInfo.Web.Pages
 {
-    public class DisplayRouteData
-    {
-        public string Description { get; set; }
-        public string ShortName { get; set; }
-        public DateTime PredictedArrival { get; set; }
-        public string DisplayArrivalTime
-        {
-            get
-            {
-                return this.PredictedArrival.ToShortTimeString();
-            }
-        }
-    }
 
     public class IndexModel : PageModel
     {
@@ -36,6 +22,7 @@ namespace BusInfo.Web.Pages
         {
             const string routeServiceURL = "http://businfo.api/api/routes";
             List<Route> routes = await _busRouteProvider.GetBusRoutesAsync<List<Route>>(routeServiceURL);
+
             this.Routes = from route in routes
                           //where route.ShortName != string.Empty && route.Description != string.Empty
                           select new DisplayRouteData
